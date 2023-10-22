6f7567ec12d0d94c81340f2e0f37399e3c7798a1
Misha <fshg@dh.com>
2023-10-22 14:53:32 +0300

    create by games

---
+++ game.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+WIDHT = 1300
+HEIGHT = 600
+
+SIZE = (WIDHT,HEIGHT)
+
+window = pygame.display.set_mode(SIZE)
+background_color = (255,0,0)
+window.fill(background_color)
+clock = pygame.time.Clock()
+
+
+
+
+
+game = True
+finish = False
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    pygame.display.update()
+    clock.tick(60)
