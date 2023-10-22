01eea909874b124aa6254a6fcea85ea0c1ee3c4b
Misha <fshg@dh.com>
2023-10-22 15:17:46 +0300

    add bird

--- game.py
+++ game.py
@@ -11,8 +11,27 @@ background_color = (255,0,0)
 window.fill(background_color)
 clock = pygame.time.Clock()
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, filename, coords, size, speed):
+        self.image = pygame.Surface(size=size, masks=(255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.center = coords
+        self.speed = speed
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Bird(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.rect.y -= self.speed*2
 
 
+
+
+bird = Bird(None, (0, HEIGHT/2), (50,50), 5)
 
 
 game = True
@@ -21,6 +40,9 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+    window.fill(background_color)
+    bird.update()
+    bird.reset()
 
     pygame.display.update()
     clock.tick(60)
