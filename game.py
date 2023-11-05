d5516a4d788ecb0744f02136278eaf12b290fdf2
Misha <fshg@dh.com>
2023-11-05 14:43:02 +0200

    finised game

--- game.py
+++ game.py
@@ -7,7 +7,14 @@ HEIGHT = 600
 SIZE = (WIDHT,HEIGHT)
 
 window = pygame.display.set_mode(SIZE)
-background_color = (255,0,0)
+background_color = (0,200,0)
+background_image = pygame.transform.scale(pygame.image.load("background.png"), SIZE)
+
+score = 0
+
+pygame.font.init()
+font2 = pygame.font.Font(None, 40)
+
 window.fill(background_color)
 clock = pygame.time.Clock()
 
@@ -33,17 +40,23 @@ class Tube(GameSprite):
     def update_up (self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDHT
+            self.rect.x = WIDHT + randint(0,100)
             self.rect = pygame.Rect(self.rect.x,self.rect.y, self.rect.width, randint(50,HEIGHT/2-50))
             self.image = pygame.transform.scale(self.original,(self.rect.width,self.rect.height))
+            global score
+            score += 1
+            self.speed = randint(3,10)
+
 
     def update_down(self):
         self.rect.x -= self.speed
         if self.rect.x <= 0:
-            self.rect.x = WIDHT
+            self.rect.x = WIDHT + randint(0,100)
             self.rect = pygame.Rect(self.rect.x, randint(HEIGHT/2, HEIGHT-100), self.rect.width, self.rect.height)
             self.image = pygame.transform.scale(self.original,(self.rect.width,self.rect.height))
-
+            global score
+            score += 1
+            self.speed = randint(3,10)
 
 bird = Bird("bird.png", (50, HEIGHT/2), (50,50), 5)
 up_tube = Tube("tube_up.png", (WIDHT+randint(0,100), 100), (75, 200), 4)
@@ -57,11 +70,12 @@ while game:
             game = False
 
     if not finish:
-        window.fill(background_color)
+        window.blit(background_image, (0,0))
+
         bird.update()
         bird.reset()
         up_tube.update_up()
-        up_tube.reset()
+        up_tube.reset() 
         down_tube.update_down()
         down_tube.reset()
 
@@ -71,6 +85,9 @@ while game:
             font1 = pygame.font.Font(None, 60)
             text = font1.render("Ти ЛОХ", True, (255,0,0))
             window.blit(text, (WIDHT/2-100, HEIGHT/2))
+
+        score_text = font2.render(str(score), True, (255,255,255))
+        window.blit(score_text, (0,0))
 
     pygame.display.update()
     clock.tick(60)
